ee43a4f67b936db66ceda6112ffb7010f7a3bc73
shivanidahiya214 <shivanidahiya329@gmail.com>
2026-06-23 19:47:24 +0530

    firt commit

---
+++ Shivani/nested.py
@@ -0,0 +1,27 @@
+#for i in range(1 , 6) :
+    #for j in range(1 , 6) :
+        #print("*" , end= " ")
+    #print()      
+
+#for i in range (1,6) :
+    #for j in range (0,i) :
+       # print ("*" , end = " ")
+   # print()    
+
+#for i in range (5,0,-1) :
+    #for j in range (0,i) :
+     #   print ("*" , end = " ")
+   # print ()
+
+#for i in range (1,6) :
+    #for j in range (1,i +1):
+        #print (j, end = " ")
+    #print ()
+
+for i in range(1 , 6) :
+    for j in range(1 , 6) :
+        if i==1 or i==5 or j==1 or j==5 :
+            print("*" , end= " ")
+        else :
+            print(" " ,  end = " ")
+    print()
