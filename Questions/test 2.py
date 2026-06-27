ce5a22d75dc10637ceb7f24a2511d02638296a4e
shivanidahiya214 <shivanidahiya329@gmail.com>
2026-06-27 12:40:26 +0530

    first commit

---
+++ Questions/test 2.py
@@ -0,0 +1,74 @@
+# question 1
+#write a program to input your name and age print a greeting ?
+#Answer
+name =  input("enter your name :")
+age = input("enter your age :")
+print ("Hello("name"){name} , your("age") {age} ")
+
+# question 2
+## Program to input two numbers and print sum, difference, product, and division
+#Answer 
+# a =int (input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+#Question 3
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# print("a=",b)
+# print("b=",a)
+
+#Question 4
+# convert ceisius to fahrenheit
+# celsius = int(input("Enter temperature in Celsius: "))
+# fahrenheit = (celsius * 9/5) + 32
+# print("{celsius}°C is equal to {fahrenheit}°")
+
+#QUESTION 5
+#calculate area of a circle from radius?
+#Answer
+# radius = float(input("Enter the radius of the circle: "))
+# area = radius * radius 
+# print("The area of the circle with radius {radius} is {area}")
+
+#QUESTION 6
+#answer
+# num = int(input("Enter a number: "))
+
+# if num % 2 == 0:
+#     print("{num} is Even")
+# else:
+#     print("{num} is Odd")
+
+
+#QUESTION 7
+#largest among three number?
+#Answer 
+
+
+#question 8
+# check leap year ?
+# answer
+
+
+
+
+
+# question 9
+# print grade from marks ?
+# answer
+marks=int(input("enter your marks:"))
+ifmarks = 90-100
+print("grade A :")
+elifmarks =80-89
+print("grade B :")
+elifmarks = 70-79
+print("grade C:")
+elifmarks = 60-69
+print("grade D :")
+elsemarks =  60
+print("grade F :")
--- Shivani/nested.py
+++ Shivani/nested.py
@@ -18,10 +18,82 @@
         #print (j, end = " ")
     #print ()
 
-for i in range(1 , 6) :
-    for j in range(1 , 6) :
-        if i==1 or i==5 or j==1 or j==5 :
-            print("*" , end= " ")
-        else :
-            print(" " ,  end = " ")
-    print()
+# for i in range(1 , 6) :
+#     for j in range(1 , 6) :
+#         if i==1 or i==5 or j==1 or j==5 :
+#             print("*" , end= " ")
+#         else :
+#             print(" " ,  end = " ")
+#     print()
+
+
+####################### STRING ###########################
+
+# str = "Hello world!"
+# print(str[0])
+# print(str[1])
+# print(str[2])
+# print(str[3])
+# print(str[4])
+# print(str[5])
+
+# def sumOfTwo(a,b) :
+#     sum = a+b
+#     print(sum)
+
+# sumOfTwo(10,20)
+
+# sumOfTwo(50,60)
+# sumOfTwo(1,4)
+
+# def sumOfTwo(a,b) :
+#     sum = a+b
+#     return sum  
+
+# print(sumOfTwo(10,20))
+
+
+# def totalMarks( m , h , e , s , ss) :
+#     total_mark = m+h+e+s+ss
+#     return total_mark
+
+# print(totalMarks(98 , 78, 69 , 76 , 68))
+
+# def totalMarks(name , m , h , e , s , ss):
+#     total_mark = m+h+e+s+ss
+#     print(name, "-> " ,  total_mark)
+
+# totalMarks("Shivani" , 98 , 76 , 57 , 89 , 76)    
+#question `1` 
+def greet() :
+    print("Hello , Students!")
+greet() 
+
+#qustions 2
+# def my_name() :
+#     print("Shivani")
+# my_name()
+
+#question 3
+# def sumOfTwo(a ,b) :
+#     sum = a+b
+#     return sum
+# print(sumOfTwo(1 ,3))
+
+#question 4
+# def check_even_odd(num) :
+#     if num % 2==0 :
+#         print("even")
+#     else: 
+#         print("odd") 
+
+# check_even_odd(5)
+ 
+
+
+#questin 5
+# def square(num) :
+#     print(num*num)
+# square(4)    
+str = "hello"
+print
