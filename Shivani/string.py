07f4655e5d32a74d66857f008880e0cdc18fa370
shivanidahiya214 <shivanidahiya329@gmail.com>
2026-06-29 11:14:27 +0530

    first commit

---
+++ Shivani/string.py
@@ -0,0 +1,14 @@
+# name = "shivani"
+# age = 22
+
+# # Positional arguments
+# formatted_string = "hello , {}! you are {} ,years old." .format(name , age)
+# print(formatted_string)
+
+# ######## string operator m slicing method
+# string = "hello"
+# print(string[ :: 3 ]) #: full character print , :: one character cap , ::- value hogi toh ulata print hoga
+# print(string[::-1]) # 0:3 = 0,1,2  # :4 start se 3 tak  # 2: 2se last tak .
+name = "akshay"
+greeting = "hello , {}! welcome to the class." .format(name)
+print(greeting)
--- TAMANNA/first.py
+++
@@ -1,6 +0,0 @@
-name = "tamanna"
-age = 17
-height = 5.1
-print(name), name
-print(age),age
-print (height),height
